3156ef8b7bf4b659bd32cbaa40d2bf5b44a6d6a5
DamienBitrise <damien.murphy@bitrise.io>
2021-04-14 19:05:47 -0700

    Initial commit

--- utils.js
+++ utils.js
@@ -121,17 +121,37 @@ BUILD
   */
   rebuild: (API_KEY, appSlug, buildData) => {
     let build = buildData.data;
+
+    let build_params = {
+      "branch": "",
+      "commit_paths": [],
+      "diff_url": "",
+      "environments": [],
+      "pull_request_author": "",
+      "pull_request_head_branch": "",
+      "pull_request_id": {},
+      "pull_request_merge_branch": "",
+      "pull_request_repository_url": "",
+      "skip_git_status_report": true,
+      "tag": "",
+      "workflow_id": ""
+    };
     console.log("Abort Reason:", build.abort_reason);
     if(build.abort_reason != "User Personal requested to abort this build."){
       console.log("Skipping Rebuild");
       return;
     }
     console.log("Rebuilding Params:",build.original_build_params);
+    let keys = Object.keys(build.original_build_params);
+    keys.forEach((key) => {
+      build_params[key] = build.original_build_params[key];
+    })
+    console.log("build_params:",build_params);
     const BUILD_URL = 'https://api.bitrise.io/v0.1/apps/' + appSlug + "/builds/";
     fetch(BUILD_URL, {
       method: 'post',
       body:    JSON.stringify({
-        "build_params": build.original_build_params,
+        "build_params": build_params,
         "hook_info": {
           "type": "abort_rebuild_bot"
         }
